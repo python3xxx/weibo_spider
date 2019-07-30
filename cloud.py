50610ff69441e25e935ef45eda18f229404a9bb0
wyl <a_wyl1994@163.com>
2019-07-31 02:45:19 +0800

    fix

--- cloud.py
+++ cloud.py
@@ -1,28 +1,25 @@
-import os
-from wordcloud import WordCloud
+# wordcloud词云  
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud, ImageColorGenerator
+import os
+import numpy as np
+import PIL.Image as Image
+
 
 path = os.getcwd() + '/comment.txt'
-# 读signature.txt文本内容
 content = open(path, encoding='utf-8').read().replace('图片评论', '')\
     .replace('回复', '').replace('等人', '')
+alice_coloring = np.array(Image.open('background.jpg'))
 
-# 词云配置
-wc = WordCloud(
-    # 背景色
-    background_color='white',
-    # 最大显示的词数
-    max_words=1000,
-    height=500,
-    width=500,
-    # worldcloud 本身不支持中文，需要指定字体文件路径
-    font_path='C:\Windows\Fonts\msyh.ttf',
-    max_font_size=60,
-    # 随机配色方案数
-    random_state=30,
-).generate(content)
+my_wordcloud = WordCloud(background_color="white", max_words=2000,
+                         mask=alice_coloring,max_font_size=40, random_state=42,
+                         font_path='C:\Windows\Fonts\msyh.ttf') \
+.generate(content)
 
-plt.imshow(wc)
-plt.axis('off')
+image_colors = ImageColorGenerator(alice_coloring)
+plt.imshow(my_wordcloud.recolor(color_func=image_colors))
+plt.imshow(my_wordcloud)
+plt.axis("off")
 plt.show()
-wc.to_file('signature_cloud.png')
+
+my_wordcloud.to_file('weibo_xiong.png')
