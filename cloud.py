f4c4a73360403ce8090c4e91a8bb71579b7191e5
wyl <a_wyl1994a@163.com>
2019-07-31 14:13:19 +0800

    test

--- cloud.py
+++ cloud.py
@@ -15,8 +15,7 @@ alice_coloring = np.array(Image.open('background.jpg'))
 
 my_wordcloud = WordCloud(background_color="white", max_words=2000,
                          mask=alice_coloring,max_font_size=40, random_state=42,
-                         font_path='C:\Windows\Fonts\msyh.ttf') \
-.generate(content)
+                         font_path='C:\Windows\Fonts\msyh.ttf').generate(content)
 
 image_colors = ImageColorGenerator(alice_coloring)
 plt.imshow(my_wordcloud.recolor(color_func=image_colors))
